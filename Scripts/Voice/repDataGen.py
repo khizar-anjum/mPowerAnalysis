84fc3c7a0fb8bb0e380a190449131596123e0c4b
khizar-anjum <khizaranjum28@gmail.com>
2019-01-08 01:01:26 +0500

    started working on tapping data

--- Scripts/Voice/repDataGen.py
+++ Scripts/Voice/repDataGen.py
@@ -8,8 +8,8 @@
 #%% Importing libararies
 import pandas as pd
 import numpy as np 
-#from pydub import AudioSegment
-#import scipy.signal as sg
+from pydub import AudioSegment
+import scipy.signal as sg
 import sys
 from dfa import dfa
 from librosa.feature import mfcc
@@ -41,7 +41,7 @@ def update_progress(progress):
     sys.stdout.write(text)
     sys.stdout.flush()
 
-"""
+
 #%% this block converts audio files into raw_audio format and decimates them
 pad = lambda a,i : a[0:i] if len(a) > i else np.concatenate((a, [0] * (i-len(a)))) #for fixing things to a fixed length
 sample_len = 22050; #sample Fs is 44100. After 20x decimation, a 10-sec recording should have 44.1k Samples
@@ -74,7 +74,17 @@ for x in v_data.index.levels[0]:
             err_samples.append((x,i));
         update_progress((i+1)/len(v_data.loc[x,:]))
     np.save('..\\..\\Data\\VoiceData\\rep20dsData\\X_'+x+'_sec'+str(k)+'.npy',X)
-"""   
+
+#%% Lets remove the erroneus samples from the rep_voice_cohort.csv
+    #this block is clumsy! hope to better this code soon!
+#np.save('..\\..\\Data\\VoiceData\\rep20dsData\\err_samples.npy',err_samples)
+err_samples = np.load('..\\..\\Data\\VoiceData\\rep20dsData\\err_samples.npy');
+X_test = v_data.loc['test'].reset_index().drop([int(x) for x in err_samples[0:3,1]])
+X_train = v_data.loc['train'].reset_index().drop([int(x) for x in err_samples[3:,1]])
+X_val = v_data.loc['val'].reset_index()
+rp20ds = pd.concat([X_train, X_val,X_test],keys=['train','val','test'])
+rp20ds.to_csv('..\\..\\Data\\VoiceData\\rep20dsData\\Seriesrep20dsData.csv')
+#v_data.to_csv()
 #%% SECTION 2 RF FEATURE GENERATION
 # this section generates a full dataset which will contain all features form Aurora et al. 
     
@@ -129,31 +139,37 @@ def find_f0(a,Fs):
 # 6. mfccs
 def feature_extractor(data,Fs):
     #expecting data to be a matrix of size (n_samples, raw_audio_length)
-    
+    err = [];
     for i,audio in enumerate(data):
-        _,temp,_ = dfa(audio); #using default values for now!
-        temp = np.hstack((temp, np.mean(tkeo(audio)))) #because mean is used in paper
-        temp = np.hstack((temp, np.reshape(mfcc(y=audio,sr=Fs,n_mfcc=40),(-1,))))
-        if(i == 0): y = temp
-        else: y = np.vstack((y,temp))
-    return y
+        try:
+            _,temp,_ = dfa(audio); #using default values for now!
+            temp = np.hstack((temp, np.mean(tkeo(audio)))) #because mean is used in paper
+            temp = np.hstack((temp, np.reshape(mfcc(y=audio,sr=Fs,n_mfcc=40),(-1,))))
+            if(i == 0): y = temp
+            else: y = np.vstack((y,temp))
+        except Exception as e:
+            print(e);
+            err.append(i);
+        update_progress((i+1)/len(data))
+    return err,y
 #%%  loads rep20dsData and extracts features using feature extractor!
-
+#CAUTION: THIS BLOCK IS NOT TESTED!
 parts = 500;
 X = [];
 err_samples = [];
 for x in v_data.index.levels[0]:
-    for i in range(int(len(v_data.loc[x,:].index)/parts)):
-        try:
-            if(i==0):X = feature_extractor(np.load('..\\..\\Data\\VoiceData\\rep20dsData\\X_'+x+'_sec0.npy'),Fs=2205)
-            else:X = np.vstack((X,feature_extractor(np.load('..\\..\\Data\\VoiceData\\rep20dsData\\X_'+x+'_sec'+str(i)+'.npy'),Fs=2205)))
-            update_progress((i+1)/int(len(v_data.loc[x,:].index)/parts))
-        except Exception as e:
-            print(e);
-            print('moving on!');
-            err_samples.append((x,i));
+    for i in range(int(len(v_data.loc[x,:].index)/parts)+1):
+        if(i==0):err, X = feature_extractor(np.load('..\\..\\Data\\VoiceData\\rep20dsData\\X_'+x+'_sec0.npy'),Fs=2205)
+        else:err, X = np.vstack((X,feature_extractor(np.load('..\\..\\Data\\VoiceData\\rep20dsData\\X_'+x+'_sec'+str(i)+'.npy'),Fs=2205)))
+        update_progress((i+1)/int(len(v_data.loc[x,:].index)/parts))
+        if(len(err)!=0):err_samples.append((err,X,i));
     np.save('..\\..\\Data\\VoiceData\\repRFData\\X_'+x+'.npy',X)
     X = [];
     print('X_'+x+'.npy saved!')
 del X
 np.save('..\\..\\Data\\VoiceData\\repRFData\\err_samples.npy',err_samples)
+#%% saving the series file for this!
+# CAUTION THIS BLOCK HAS ALSO NOT BEEN TESTED!
+rfseries = pd.read_csv('..\\..\\Data\\VoiceData\\rep20dsData\\Seriesrep20dsData.csv',index_col = [0,1])
+rfseries = rfseries.drop(index=('train',3701)) #derived from err_samples
+rfseries.to_csv('..\\..\\Data\\VoiceData\\repRFData\\SeriesrepRFData.csv')
--- Scripts/Voice/repRFanalysis.py
+++ Scripts/Voice/repRFanalysis.py
@@ -7,8 +7,42 @@ Created on Sun Jan  6 22:59:36 2019
 #this file is meant for RF application on repRFData
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, f1_score, average_precision_score
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 
+#%% Importing our datasets!
+X_val = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_val.npy')
+X_train = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_train.npy')
+X_test = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_test.npy')
+
+#%% Importing labels
+series_data = pd.read_csv('..\\..\\Data\\VoiceData\\repRFData\\SeriesrepRFData.csv',index_col = [0,1])
+Y_train = series_data.loc['train',:]['professional-diagnosis'].values
+Y_val = series_data.loc['val',:]['professional-diagnosis'].values
+Y_test = series_data.loc['test',:]['professional-diagnosis'].values
+#%%
+le = preprocessing.LabelEncoder()
+Y_train = le.fit_transform(Y_train)
+Y_val = le.fit_transform(Y_val)
+Y_test = le.fit_transform(Y_test)
 
 #%%
+rfclf = RandomForestClassifier(n_estimators = 1024, max_depth = 5, random_state = 42, max_features = 'log2')
+rfclf.fit(X_train,Y_train)
+y_pred = rfclf.predict(X_test);
 
-X = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_val.npy')
+#%%
+y_prob = rfclf.predict_proba(X_test);
+print('AUC ROC:',roc_auc_score(Y_test,y_prob[:,1]))
+print('AUC PR:',average_precision_score(Y_test,y_prob[:,1]))
+print('F1:',f1_score(Y_test,y_pred))
+print('Accuracy:',accuracy_score(Y_test,y_pred))
+print(rfclf.score(X_test,Y_test))
+
+#%%
+fpr, tpr, thresholds = roc_curve(Y_test,y_prob[:,1])
+roc_auc = auc(fpr,tpr)
+plt.plot(fpr,tpr,lw=1,alpha=0.3,label='ROC (AUC = %0.2f)'%roc_auc)
