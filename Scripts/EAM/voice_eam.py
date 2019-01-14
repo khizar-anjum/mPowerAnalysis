0f930df12c49f07ddc076977e32c6468ada1f76c
khizar-anjum <khizaranjum28@gmail.com>
2019-01-14 06:20:22 +0500

    compeleted initial EAM analysis

--- Scripts/EAM/voice_eam.py
+++ Scripts/EAM/voice_eam.py
@@ -16,9 +16,9 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
-from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.layers import Dense, Dropout, LSTM, Bidirectional
+from scipy import stats
 
 #%% Importing the mfccData which is indexed correctly now!
 X_train = np.load('..\\..\\Data\\VoiceData\\mfccData\\X_mfcc_train_cind.npy')
@@ -45,7 +45,7 @@ rfclf = RandomForestClassifier(n_estimators = 150, max_depth = 25, random_state 
 rfclf.fit(X_train,Y_train)
 print(rfclf.score(X_test,Y_test))
 print(rfclf.score(X_train,Y_train))
-
+#test comes out to be 0.6241933063184751
 #%% Calculating next inputs and labels
 X_train_eam = pd.DataFrame(index= mfccSeries['healthCode'][mfccSeries['split'] == 'train'],\
                            data = rfclf.predict(X_train))
@@ -60,8 +60,8 @@ Y_test_eam = mfccSeries[mfccSeries['split'] == 'test'].drop_duplicates(subset = 
 
 #%% Making the model!
 model = Sequential()
-model.add(Bidirectional(LSTM(16,return_sequences=True)))
-model.add(Bidirectional(LSTM(16)))
+model.add(Bidirectional(LSTM(16),input_shape=(None,1))) #
+#model.add(Bidirectional(LSTM(16)))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -70,7 +70,110 @@ model.summary()
 #%% Lets train the model
 for i,j in enumerate(np.unique(X_train_eam.index.values)):
     print('Training on %d sample:'%(i+1))
-    model.fit(np.squeeze(X_train_eam.loc[j].values), int(Y_train_eam.iloc[i]),\
+    if(np.ndim(X_train_eam.loc[j].values) == 2): train_in = np.squeeze(X_train_eam.loc[j].values)
+    else: train_in = X_train_eam.loc[j].values
+    model.fit(np.expand_dims(np.expand_dims(train_in,axis=0),axis=2), \
+              np.expand_dims(np.expand_dims(np.array(int(Y_train_eam.iloc[i])),axis=0),axis=1),\
           batch_size=10,\
           epochs=10,\
-          validation_data=[np.squeeze(X_val_eam.loc[X_val_eam.index[0]].values), int(Y_val_eam.iloc[0])])
+          validation_data=(np.expand_dims(np.expand_dims(np.squeeze(X_val_eam.loc[X_val_eam.index[0]].values),axis=0),axis=2),\
+                           np.expand_dims(np.expand_dims(np.squeeze(np.array(int(Y_val_eam.iloc[0]))),axis=0),axis=1)))
+#%% Predictions from the model
+y_pred = []
+for i,j in enumerate(np.unique(X_test_eam.index.values)):
+    if(np.ndim(X_test_eam.loc[j].values) == 2): test_in = np.squeeze(X_test_eam.loc[j].values)
+    else: test_in = X_test_eam.loc[j].values
+    y_pred.append(model.predict(np.expand_dims(np.expand_dims(test_in,axis=0),axis=2)))
+    
+#%% lets check the predictions!
+y_pred = np.round(np.ravel(y_pred))    
+Y_test_eam = le.fit_transform(Y_test_eam.values)
+print(((Y_test_eam - y_pred)**2).sum()/len(y_pred))
+#comes out to be 0.3140495867768595
+#%% The mode predictions
+y_pred = []
+for i,j in enumerate(np.unique(X_test_eam.index.values)):
+    temp,_ = stats.mode(np.squeeze(X_test_eam.loc[j].values))
+    y_pred.append(temp)
+y_pred = np.round(np.ravel(y_pred))   
+print(((Y_test_eam - y_pred)**2).sum()/len(y_pred))
+#comes out to be ~0.31074380165289256
+#%% SECTION 2:
+"""
+THIS IS THE SECOND SECTION OF THIS PYTHON FILE AND IT DEALS WITH THE APPLICATION
+OF THE LSTM EAM ON THE repRFData. 
+IT DOES NOT SHARE ANYTHING WITH ALL THE ABOVE SECTIONS WITH THE EXCEPTION OF IMPORTING
+LIBRARIES
+"""
+#%% Importing our datasets!
+X_val = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_val.npy')
+X_train = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_train.npy')
+X_test = np.load('..\\..\\Data\\VoiceData\\repRFData\\X_test.npy')
+
+#%% Importing labels
+series_data = pd.read_csv('..\\..\\Data\\VoiceData\\repRFData\\SeriesrepRFData.csv',index_col = [0,1])
+Y_train = series_data.loc['train',:]['professional-diagnosis'].values
+Y_val = series_data.loc['val',:]['professional-diagnosis'].values
+Y_test = series_data.loc['test',:]['professional-diagnosis'].values
+#%%
+le = preprocessing.LabelEncoder()
+Y_train = le.fit_transform(Y_train)
+Y_val = le.fit_transform(Y_val)
+Y_test = le.fit_transform(Y_test)
+
+#%%
+rfclf = RandomForestClassifier(n_estimators = 1024, max_depth = 5, random_state = 42, max_features = 'log2')
+rfclf.fit(X_train,Y_train)
+print(rfclf.score(X_test,Y_test))
+# comes out to be 0.5847076461769115
+#%%
+X_train_eam = pd.DataFrame(index= series_data.loc['train',:]['healthCode'],\
+                           data = rfclf.predict(X_train))
+X_val_eam = pd.DataFrame(index= series_data.loc['val',:]['healthCode'],\
+                         data = rfclf.predict(X_val))
+X_test_eam = pd.DataFrame(index= series_data.loc['test',:]['healthCode'],\
+                          data = rfclf.predict(X_test))
+
+Y_train_eam = series_data.loc['train',:].drop_duplicates(subset = 'healthCode')['professional-diagnosis']
+Y_val_eam = series_data.loc['val',:].drop_duplicates(subset = 'healthCode')['professional-diagnosis']
+Y_test_eam = series_data.loc['test',:].drop_duplicates(subset = 'healthCode')['professional-diagnosis']
+
+#%% Making the model!
+model = Sequential()
+model.add(Bidirectional(LSTM(16),input_shape=(None,1))) #
+#model.add(Bidirectional(LSTM(16)))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+#%% Lets train the model
+for i,j in enumerate(np.unique(X_train_eam.index.values)):
+    print('Training on %d sample:'%(i+1))
+    if(np.ndim(X_train_eam.loc[j].values) == 2): train_in = np.squeeze(X_train_eam.loc[j].values)
+    else: train_in = X_train_eam.loc[j].values
+    model.fit(np.expand_dims(np.expand_dims(train_in,axis=0),axis=2), \
+              np.expand_dims(np.expand_dims(np.array(int(Y_train_eam.iloc[i])),axis=0),axis=1),\
+          batch_size=10,\
+          epochs=10,\
+          validation_data=(np.expand_dims(np.expand_dims(np.squeeze(X_val_eam.loc[X_val_eam.index[0]].values),axis=0),axis=2),\
+                           np.expand_dims(np.expand_dims(np.squeeze(np.array(int(Y_val_eam.iloc[0]))),axis=0),axis=1)))
+#%% Predictions from the model
+y_pred = []
+for i,j in enumerate(np.unique(X_test_eam.index.values)):
+    if(np.ndim(X_test_eam.loc[j].values) == 2): test_in = np.squeeze(X_test_eam.loc[j].values)
+    else: test_in = X_test_eam.loc[j].values
+    y_pred.append(model.predict(np.expand_dims(np.expand_dims(test_in,axis=0),axis=2)))
+#%% lets check the predictions!
+y_pred = np.round(np.ravel(y_pred))    
+Y_test_eam = le.fit_transform(Y_test_eam.values)
+print(((Y_test_eam - y_pred)**2).sum()/len(y_pred))
+#comes out to be about 0.5098814229249012
+#%%
+y_pred = []
+for i,j in enumerate(np.unique(X_test_eam.index.values)):
+    temp,_ = stats.mode(np.squeeze(X_test_eam.loc[j].values))
+    y_pred.append(temp)
+y_pred = np.round(np.ravel(y_pred))    
+print(((Y_test_eam - y_pred)**2).sum()/len(y_pred))
+#comes out to be 0.48221343873517786
